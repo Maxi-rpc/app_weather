7a8bb4d5fae7f505a32ba6e48b1b8e754a02d1be
Maximiliano <maxirpc2607@gmail.com>
2023-10-20 09:45:10 -0300

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -14,6 +14,7 @@ import icon_luna from "@/public/icons/icon_luna.svg";
 import icon_luna_nublado from "@/public/icons/icon_luna_nublado.svg";
 
 const icons = {
+	Clear: icon_sol,
 	Clouds: icon_sol_nublado,
 	Rain: icon_lluvia,
 };
@@ -71,7 +72,69 @@ export default function Home() {
 				<Divider />
 				{/* insert city */}
 				<div className="w-full">
-					<div className="w-1/2"></div>
+					<div className="w-1/2">
+						<CardMain>
+							<form action="">
+								<div className="grid grid-cols-1 gap-x-6 gap-y-8 md:grid-cols-3 sm:grid-cols-3">
+									<div className="sm:col-span-full">
+										<div className="sm:col-span-2 sm:col-start-1">
+											<label
+												htmlFor="city"
+												className="block text-sm font-medium leading-6 text-gray-900"
+											>
+												City
+											</label>
+											<div className="mt-2">
+												<input
+													type="text"
+													name="city"
+													id="city"
+													autoComplete="address-level2"
+													className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+												/>
+											</div>
+										</div>
+
+										<div className="sm:col-span-2">
+											<label
+												htmlFor="region"
+												className="block text-sm font-medium leading-6 text-gray-900"
+											>
+												State / Province
+											</label>
+											<div className="mt-2">
+												<input
+													type="text"
+													name="region"
+													id="region"
+													autoComplete="address-level1"
+													className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+												/>
+											</div>
+										</div>
+
+										<div className="sm:col-span-2">
+											<label
+												htmlFor="postal-code"
+												className="block text-sm font-medium leading-6 text-gray-900"
+											>
+												ZIP / Postal code
+											</label>
+											<div className="mt-2">
+												<input
+													type="text"
+													name="postal-code"
+													id="postal-code"
+													autoComplete="postal-code"
+													className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+												/>
+											</div>
+										</div>
+									</div>
+								</div>
+							</form>
+						</CardMain>
+					</div>
 				</div>
 				<Divider />
 				{/* card */}
@@ -97,7 +160,7 @@ export default function Home() {
 									src={icons[weather.main]}
 									width={200}
 									height={200}
-									alt={icons[weather.main]}
+									alt={weather.main}
 								/>
 							</div>
 						</div>
