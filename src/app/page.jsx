971e6a0b0ba8e41ab614e1c56efbf19ddaec0224
Maximiliano <maxirpc2607@gmail.com>
2023-10-21 19:27:27 -0300

    Update page.jsx
    Se agregan inputs styles

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -72,69 +72,50 @@ export default function Home() {
 				<Divider />
 				{/* insert city */}
 				<div className="w-full">
-					<div className="w-1/2">
-						<CardMain>
-							<form action="">
-								<div className="grid grid-cols-1 gap-x-6 gap-y-8 md:grid-cols-3 sm:grid-cols-3">
-									<div className="sm:col-span-full">
-										<div className="sm:col-span-2 sm:col-start-1">
-											<label
-												htmlFor="city"
-												className="block text-sm font-medium leading-6 text-gray-900"
-											>
-												City
-											</label>
-											<div className="mt-2">
-												<input
-													type="text"
-													name="city"
-													id="city"
-													autoComplete="address-level2"
-													className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-												/>
-											</div>
-										</div>
-
-										<div className="sm:col-span-2">
-											<label
-												htmlFor="region"
-												className="block text-sm font-medium leading-6 text-gray-900"
-											>
-												State / Province
-											</label>
-											<div className="mt-2">
-												<input
-													type="text"
-													name="region"
-													id="region"
-													autoComplete="address-level1"
-													className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-												/>
-											</div>
-										</div>
-
-										<div className="sm:col-span-2">
-											<label
-												htmlFor="postal-code"
-												className="block text-sm font-medium leading-6 text-gray-900"
-											>
-												ZIP / Postal code
-											</label>
-											<div className="mt-2">
-												<input
-													type="text"
-													name="postal-code"
-													id="postal-code"
-													autoComplete="postal-code"
-													className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-												/>
-											</div>
-										</div>
-									</div>
+					<CardMain>
+						<form
+							className="flex justify-center space-x-6 w-full"
+							action="#"
+							method="POST"
+						>
+							<div className="w-1/3">
+								<div className="mt-2">
+									<input
+										id="country"
+										name="country"
+										type="text"
+										placeholder="Country"
+										required
+										className="block w-full rounded-md border-0 py-2 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+									/>
 								</div>
-							</form>
-						</CardMain>
-					</div>
+							</div>
+
+							<div className="w-1/3">
+								<div className="mt-2">
+									<input
+										id="province"
+										name="province"
+										type="text"
+										placeholder="Province"
+										required
+										className="block w-full rounded-md border-0 py-2 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+									/>
+								</div>
+							</div>
+
+							<div className="w-1/3">
+								<div className="mt-2">
+									<button
+										type="submit"
+										className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+									>
+										Search
+									</button>
+								</div>
+							</div>
+						</form>
+					</CardMain>
 				</div>
 				<Divider />
 				{/* card */}
