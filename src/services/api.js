69bf36c44593c8fc1f8275af663374717c5b3681
Maximiliano <maxirpc2607@gmail.com>
2023-10-25 10:44:35 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,10 @@
 import { API_KEY } from "@/settings/keys";
 import axios from "axios";
 
+// const API_KEY = process.env.API_KEY;
+
 export const get_data = (country, city) => {
+	//console.log(API_KEY);
 	const apiUrl = "https://api.openweathermap.org/data/2.5/weather";
 	const query = `${apiUrl}?q=${city},${country}&appid=${API_KEY}&units=metric&lang=es`;
 	return axios.get(query);
